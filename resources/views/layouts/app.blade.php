f3d37522e0845bcd5f9fbbd26cc1c4dac42f9cae
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-12-21 08:55:02 +0300

    Remove search bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,14 +38,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <form class="form-inline d-flex justify-content-center md-form form-sm active-purple active-purple-2 mt-2"  action="{{ route('search') }}" method="GET">
-                            <input type="text" class="form-control form-control-sm ml-3 w-75" name="search"  placeholder="&#xF002;   Search" style="font-family:Arial, FontAwesome" style="text-align:center;width:800px;height:25px; margin-top:5px; border: 1px solid #A0AEC0;">
-                        </form>
-                    </ul>
-
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">                
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto" style="color: #388087;">
                         <!-- Authentication Links -->
