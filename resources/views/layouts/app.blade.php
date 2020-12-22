31faaa1920a242c5fece2b0c6c9b97010ddd5c02
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2020-12-22 09:25:11 -0800

    Administrator navbar url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,9 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('profile',Auth::user()->id) }}">My Profile</a>
                                     <a class="dropdown-item" href="{{ route('users.edit',Auth::user()->id) }}">Edit Profile</a>
+                                    @role('administrator')
+                                    <a class="dropdown-item" href="{{ route('admin-panel',Auth::user()->id) }}">Admin Panel</a>
+                                    @endrole
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
